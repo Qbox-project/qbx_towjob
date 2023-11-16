f717159b2559317034bdab6d2a706a913beb0f77
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:57:38 -0500

    fix: import modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,25 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-TowJob'
+description 'QBX_TowJob'
+repository 'https://github.com/Qbox-project/qbx_towjob'
 version '1.0.0'
 
 shared_scripts {
 	'@ox_lib/init.lua',
-	'@qb-core/shared/locale.lua',
-	'config.lua',
+	'@qbx_core/modules/utils.lua',
+	'@qbx_core/shared/locale.lua',
 	'locales/en.lua',
+	'locales/*.lua',
+	'config.lua'
 }
 
 client_scripts {
+	'@qbx_core/modules/playerdata.lua',
 	'@PolyZone/client.lua',
 	'@PolyZone/BoxZone.lua',
 	'@PolyZone/ComboZone.lua',
-	'client/main.lua'
+	'client/main.lua',
 }
 
 server_script 'server/main.lua'
